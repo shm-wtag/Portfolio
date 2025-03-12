951b7cff9ed9c26ea97bc563b230d61890049eac
Maruf Shafique <maruf.shafique@welldev.io>
2025-03-12 14:05:59 +0600

    responsive send message

--- components/InputApp.vue
+++ components/InputApp.vue
@@ -3,6 +3,7 @@ interface Props {
   label: string;
   placeholder: string;
   textarea?: boolean;
+  rows?: number;
 }
 
 withDefaults(defineProps<Props>(), {
@@ -13,13 +14,14 @@ withDefaults(defineProps<Props>(), {
 <template>
   <div>
     <label
-      class="text-xs font-light focus-within:text-blue-600 transition-all duration-150 capitalize"
+      class="text-xs font-light focus-within:text-blue-600 transition-all duration-150 capitalize sm:text-sm"
     >
       <span v-text="label" />
 
       <textarea
         v-if="textarea"
         :placeholder="placeholder"
+        :rows="rows"
         class="w-full py-1 text-sm outline-none border-b border-gray-600 bg-transparent focus:border-blue-600 font-light transition-all duration-150 text-gray-800"
       />
       <input
--- components/MessageSection.vue
+++ components/MessageSection.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="space-y-2">
-    <h1 class="font-semibold text-3xl text-primary">Send me a message!</h1>
-    <p class="text-sm">
-      Got a question or proposal, or just want to say hello? Go ahead.
-    </p>
+  <div class="space-y-2 mx-auto sm:w-2/3 sm:h-screen sm:pt-64">
+    <div class="text-center">
+      <h1 class="font-semibold text-3xl text-primary sm:text-4xl">
+        Send me a message!
+      </h1>
+      <p class="text-sm text-accent pt-2 sm:text-lg">
+        Got a question or proposal, or just want <br />
+        to say hello? Go ahead.
+      </p>
+    </div>
 
-    <form action="" class="pt-4 space-y-4">
-      <InputApp label="Your name" placeholder="Enter your name" />
+    <form action="" class="pt-8 space-y-4">
+      <div class="gap-8 sm:flex">
+        <InputApp
+          class="w-full pb-4"
+          label="Your name"
+          placeholder="Enter your name"
+        />
 
-      <InputApp label="Email address" placeholder="Enter your email address" />
+        <InputApp
+          class="w-full"
+          label="Email address"
+          placeholder="Enter your email address"
+        />
+      </div>
+
       <InputApp
+        class="pb-8"
         :textarea="true"
         label="Email message"
         placeholder="Enter your email address"
+        :rows="4"
       />
 
       <Button variant="reveal" size="reveallg">
