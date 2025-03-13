68277024d12506c62a2e6c3a77e302e1db9f4c17
Maruf Shafique <maruf.shafique@welldev.io>
2025-03-13 17:06:22 +0600

    fix build padding

--- components/BuildSection.vue
+++ components/BuildSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="build" class="pt-20 sm:h-screen lg:pt-80">
     <div class="bg-white mx-auto w-4/5 lg:w-3/4 lg:flex lg:justify-between">
-      <div class="p-16 space-y-4 lg:p-20 lg:w-1/2 lg:space-y-8">
+      <div class="p-10 space-y-4 lg:p-20 lg:w-1/2 lg:space-y-8">
         <h1 class="text-3xl text-primary font-semibold lg:text-4xl">
           I build & <br />
           design stuff
@@ -25,7 +25,7 @@
         orientation="vertical"
       />
 
-      <div class="p-16 space-y-4 lg:p-20 lg:w-1/2 lg:space-y-8">
+      <div class="p-10 space-y-4 lg:p-20 lg:w-1/2 lg:space-y-8">
         <h1 class="text-3xl text-primary font-semibold lg:text-4xl">
           I write, <br />
           sometimes
